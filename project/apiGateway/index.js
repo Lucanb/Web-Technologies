b90dc2109507f829ac0a3a0dd6d9efb3c9463de3
Lucanb <lucanastasa@yahoo.com>
2024-05-21 09:20:54 +0300

    am facut un model de api gateway

---
+++ project/apiGateway/index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer({});
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    if (req.url.startsWith('/luca-app/service1')) {
+        proxy.web(req, res, { target: 'http://localhost:3001' });
+    } else if (req.url.startsWith('/luca-app/service2')) {
+        proxy.web(req, res, { target: 'http://localhost:3002' });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+const port = 5000;
+server.listen(port, () => {
+    console.log(`API Gateway is running on http://luca-app:${port}`);
+});
