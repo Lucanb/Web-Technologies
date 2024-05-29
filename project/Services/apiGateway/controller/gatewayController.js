7c6b2f71f643d35e0912bd2599432130cdba2142
Lucanb <lucanastasa@yahoo.com>
2024-05-29 23:42:21 +0300

    cred ca asa ramane

--- project/Services/apiGateway/controller/gatewayController.js
+++ project/Services/apiGateway/controller/gatewayController.js
@@ -1,16 +1,8 @@
-const { aggregateResponses, proxyRequest } = require('../service/gatewayService');
+const { proxyRequest } = require('../service/gatewayService');
 
 const routeRequest = (req, res) => {
     if (req.url.startsWith('/luca-app/auth/')) {
         proxyRequest(req, res, 'http://localhost:3000');
-    // } else if (req.url.startsWith('/luca-app/main/announces') ||
-    //     req.url.startsWith('/luca-app/main/exploreActors') ||
-    //     req.url.startsWith('/luca-app/main/toppicks') ||
-    //     req.url.startsWith('/luca-app/main/todayActors') ||
-    //     req.url.startsWith('/luca-app/main/topPicksWeek') ||
-    //     req.url.startsWith('/luca-app/main/topFavorites') ||
-    //     req.url.startsWith('/luca-app/main/commingSoon')) {
-    //     aggregateResponses(req, res, ['http://localhost:3001']);
     } else if (req.url.startsWith('/luca-app/main/')) {
         proxyRequest(req, res, 'http://localhost:3001');
     } else if (req.url.startsWith('/luca-app/admin/')) {
--- project/Services/apiGateway/service/gatewayService.js
+++ project/Services/apiGateway/service/gatewayService.js
@@ -1,5 +1,4 @@
 const httpProxy = require('http-proxy');
-const fetch = require('node-fetch');
 
 const proxy = httpProxy.createProxyServer({});
 
@@ -11,21 +10,4 @@ const proxyRequest = (req, res, target) => {
     proxy.web(req, res, { target });
 };
 
-const aggregateResponses = async (req, res, targets) => {
-    try {
-        const responses = await Promise.all(targets.map(target => fetch(target + req.url)));
-        const data = await Promise.all(responses.map(response => response.json()));
-
-        const aggregatedData = data.reduce((acc, curr) => {
-            return { ...acc, ...curr };
-        }, {});
-
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(aggregatedData));
-    } catch (error) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Error in aggregating responses');
-    }
-};
-
-module.exports = { aggregateResponses, proxyRequest };
+module.exports = { proxyRequest };
