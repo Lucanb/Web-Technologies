0b1e56fcafe023a42ae413028cf4242939f5764c
Lucanb <lucanastasa@yahoo.com>
2024-06-01 15:07:39 +0300

    done

---
+++ project/Services/FrontendService/Frontend/public/js/test.js
@@ -0,0 +1,16 @@
+// scripts.js
+
+// Function to display a greeting message in the console
+function greetUser() {
+    console.log("Hello, welcome to our website!");
+}
+
+// Function to display the current date and time in the console
+function showDateTime() {
+    const now = new Date();
+    console.log("Current Date and Time: " + now.toLocaleString());
+}
+
+// Call the functions
+greetUser();
+showDateTime();
