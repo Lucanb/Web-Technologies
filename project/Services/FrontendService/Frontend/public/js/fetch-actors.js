1de019985fc6d22d5956fe3489ee04ab7b23f039
iIonel <45739581+iIonel@users.noreply.github.com>
2024-06-02 12:28:58 +0300

    refactor

--- project/Services/FrontendService/Frontend/public/js/fetch-actors.js
+++ project/Services/FrontendService/Frontend/public/js/fetch-actors.js
@@ -41,4 +41,30 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         return "";
     }
+
+    async function redirectToActor(id) {
+        if (!id) return;
+
+        let formData = new URLSearchParams();
+        formData.append('id', id);
+
+        try {
+            const response = await fetch('/luca-app/main/send-actor-id', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: formData
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            window.location.href = `/luca-app/front/actor-profile/${id}`;
+        } catch (error) {
+            console.error('Error:', error);
+            alert('An error occurred: ' + error.message);
+        }
+    }
 });
